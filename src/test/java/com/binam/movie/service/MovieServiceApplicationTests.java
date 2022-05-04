b59fbbb4ce0bbf260b5b9e57fd2c2d5134bbcfb7
18572 <18572@DESKTOP-75GE83D>
2022-05-04 18:40:23 -0400

    Adding test

--- src/test/java/com/binam/movie/service/MovieServiceApplicationTests.java
+++ src/test/java/com/binam/movie/service/MovieServiceApplicationTests.java
@@ -1,13 +1,22 @@
 package com.binam.movie.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootTest
 class MovieServiceApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(MovieServiceApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(true, true);
 	}
 
 }
