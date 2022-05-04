13eed44566fc1ffa6534561ba7b485ef9bcbba3f
18572 <18572@DESKTOP-75GE83D>
2022-05-04 18:45:51 -0400

    Removing the bug

--- src/test/java/com/binam/movie/service/MovieServiceApplicationTests.java
+++ src/test/java/com/binam/movie/service/MovieServiceApplicationTests.java
@@ -3,12 +3,14 @@ package com.binam.movie.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class MovieServiceApplicationTests {
 	
 	public static Logger logger = LoggerFactory.getLogger(MovieServiceApplicationTests.class);
