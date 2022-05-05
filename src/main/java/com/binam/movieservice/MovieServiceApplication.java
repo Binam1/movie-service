29d9eb2d98b63c610731658477f65cfaf5d14a60
18572 <18572@DESKTOP-75GE83D>
2022-05-04 21:50:03 -0400

    build fail check

--- src/main/java/com/binam/movieservice/MovieServiceApplication.java
+++ src/main/java/com/binam/movieservice/MovieServiceApplication.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class MovieServiceApplication {
 	
-	public static Logger logger = LoggerFactory.getLogger(MovieServiceApplication.class);
+	public Logger logger = LoggerFactory.getLogger(MovieServiceApplication.class);
 	@GetMapping
 	public String welcome() {
 		return "Welcome to my page!";
