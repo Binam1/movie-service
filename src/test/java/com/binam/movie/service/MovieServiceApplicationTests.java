ab89bcc5f2514afe1693dd77ae2336f8cc7f8aa9
18572 <18572@DESKTOP-75GE83D>
2022-05-04 21:33:07 -0400

    clean up

--- src/test/java/com/binam/movie/service/MovieServiceApplicationTests.java
+++ src/test/java/com/binam/movie/service/MovieServiceApplicationTests.java
@@ -3,11 +3,9 @@ package com.binam.movie.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes=MovieServiceApplicationTests.class)
 //@RunWith(SpringRunner.class)
