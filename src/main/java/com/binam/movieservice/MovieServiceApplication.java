a3ae426dd11c58e4fad741f1e5723cf87f32288f
18572 <18572@DESKTOP-75GE83D>
2022-05-04 21:43:34 -0400

    Email notification check

--- src/main/java/com/binam/movieservice/MovieServiceApplication.java
+++ src/main/java/com/binam/movieservice/MovieServiceApplication.java
@@ -1,20 +1,25 @@
 package com.binam.movieservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @SpringBootApplication
 public class MovieServiceApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(MovieServiceApplication.class);
 	@GetMapping
 	public String welcome() {
 		return "Welcome to my page!";
 	}
 
 	public static void main(String[] args) {
+		logger.info("Main method executing...");
 		SpringApplication.run(MovieServiceApplication.class, args);
 	}
 
